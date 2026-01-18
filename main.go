93af61f3ec8a37ebbaac5a6070ba06b4405354d2
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-18 13:38:21 -0300

    feat: finishing the simplest website ever

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -9,8 +10,29 @@ import (
 )
 
 func main() {
-	godotenv.Load(".env")
+	_ = godotenv.Load(".env")
 	port := os.Getenv("PORT")
+
+	http.Handle("/public/",
+		http.StripPrefix("/public/",
+			http.FileServer(http.Dir("public")),
+		),
+	)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mainT, err := template.ParseFiles("internal/templates/main-page.html.tmpl")
+		if err != nil {
+			log.Println("[ERROR] could parse html file: ", err)
+			http.Error(w, "Something went really wrong", http.StatusInternalServerError)
+			return
+		}
+
+		if err = mainT.Execute(w, nil); err != nil {
+			log.Println("[ERROR] Couldn't parse the web page: ", err)
+			http.Error(w, "Something went really wrong", http.StatusInternalServerError)
+			return
+		}
+	})
 
 	log.Printf("Application running on %s\n", port)
 	log.Fatalln(http.ListenAndServe(":"+port, nil))
